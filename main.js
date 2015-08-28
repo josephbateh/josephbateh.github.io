8c0dfecf4507847bc2d065ca47ac655d78402482
Joseph Bateh <josephbateh@gmail.com>
2015-08-28 17:37:34 -0400

    Loading Bar

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+
+
 (function() {
-  
+  var percent = 0;
+  var loadingBar = document.getElementById("loadingBar");
+  var loadingText = document.getElementById("loadingText");
+
+  window.setInterval(changeLoad, 100);
+
+  function changeLoad() {
+    percent = percent + 2;
+    if(percent >= 100) {
+      loadingBar.style.width = "100%";
+      loadingText.innerText = "Completed!";
+      loadingBar.className = "progress-bar progress-bar-info progress-bar-striped";
+      window.clearInterval(changeLoad);
+      document.getElementById("uniqueId").style.display = "block";
+      return;
+    }
+    loadingBar.style.width = percent + "%";
+    loadingText.innerText = percent + "%";
+  }
 })();
